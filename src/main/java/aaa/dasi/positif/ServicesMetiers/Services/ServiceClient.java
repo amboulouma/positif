97e4c56b2f05de30b85acf093eebac8a762381f0
aelomarial <aelomarial@IF501-202-15.insa-lyon.fr>
2018-03-07 18:08:40 +0100

    Ajout service inscription client (+ modif clientDao)

--- src/main/java/aaa/dasi/positif/ServicesMetiers/Services/ServiceClient.java
+++ src/main/java/aaa/dasi/positif/ServicesMetiers/Services/ServiceClient.java
@@ -5,10 +5,26 @@
  */
 package aaa.dasi.positif.ServicesMetiers.Services;
 
+import aaa.dasi.positif.DAO.ClientDAO;
+import aaa.dasi.positif.DAO.JpaUtil;
+import aaa.dasi.positif.ServicesMetiers.Modeles.AdressePostale;
+import aaa.dasi.positif.ServicesMetiers.Modeles.Client;
+import aaa.dasi.positif.ServicesMetiers.Modeles.ProfilAstrologique;
+import java.util.Date;
+import javax.persistence.EntityManager;
+
 /**
  *
  * @author aelomarial
  */
-public class ServiceClient {
-    
+public class ServiceClient{
+    public boolean inscriptionClient(Client client){
+        boolean resultat = false;
+        JpaUtil.creerEntityManager();
+        JpaUtil.ouvrirTransaction();
+        ClientDAO clientDao = new ClientDAO();
+        clientDao.persist(client);
+        JpaUtil.validerTransaction();
+        return resultat;
+    }
 }
