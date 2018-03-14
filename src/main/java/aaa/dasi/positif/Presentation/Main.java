2833b4470e7959b533d1796aea4b26dbf5e6027e
aelomarial <aelomarial@IF501-202-15.insa-lyon.fr>
2018-03-14 14:36:59 +0100

    Ajout de services, main

--- src/main/java/aaa/dasi/positif/Presentation/Main.java
+++ src/main/java/aaa/dasi/positif/Presentation/Main.java
@@ -8,6 +8,7 @@ package aaa.dasi.positif.Presentation;
 import aaa.dasi.positif.ServicesMetiers.Modeles.AdressePostale;
 import aaa.dasi.positif.ServicesMetiers.Modeles.Client;
 import aaa.dasi.positif.ServicesMetiers.Modeles.ProfilAstrologique;
+import aaa.dasi.positif.ServicesMetiers.Services.ServiceClient;
 import java.util.Date;
 
 /**
@@ -15,10 +16,11 @@ import java.util.Date;
  * @author aelomarial
  */
 public class Main {
-    public static void main(){
+    public static void main(String[] args){
         AdressePostale adressePostale = new AdressePostale("302 avenue Albert Einstein","Villeurbanne","France","69100");
         ProfilAstrologique profilAstrologique = new ProfilAstrologique("Signe du zodiaque","Signe Astrologique","Couleur porte bonheur","Animal Totem");
         Date dateNaissance = new Date(1997,12,01);
-        Client client = new Client("Tom", "Martin", "M", dateNaissance, adressePostale, "06 12 03 07 78", "tom@martin.gs", profilAstrologique);
+        Client client = new Client("Tom", "Martin", "M", dateNaissance, adressePostale, "06 12 03 07 78", "tom@martin.gs", profilAstrologique);        
+        ServiceClient.inscriptionClient(client);
     }
 }
